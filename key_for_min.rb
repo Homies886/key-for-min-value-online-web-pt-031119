464820e0e111c5fec1b0036b48c5716e0552fa59
Nicholas Rodriguez <niko876@hotmail.com>
2019-03-22 02:49:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,29 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  array = []
   if name_hash == {}
     return nil
   else
     name_hash.collect do |k, v|
-      v
-      
-  end
-end
-
-
-
-
-def key_for_min_value(name_hash)
-  if name_hash == {}
-    return nil
-  else
-    counter = 0
-    while counter < v
-      puts "#{counter}"
-    else
-      return counter
-    counter += 1
-    end
+      k.values_at
   end
 end
