0311f2aa8306d19f7ea3d52b75ead61d56ab7784
Nicholas Rodriguez <niko876@hotmail.com>
2019-03-22 03:10:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,14 @@
 def key_for_min_value(name_hash)
   smallest_value = 5000000
   smallest_key = 500000
-  name_hash.each do |k, v|
-    if v < smallest_value
-      smallest_value = v
-      smallest_key = k
+  if name_hash == {}
+    return nil
+  else
+    name_hash.each do |k, v|
+      if v < smallest_value
+        smallest_value = v
+        smallest_key = k
+      end
     end
   end
   smallest_key
